b253c0639d161ebae3fede3d4899e4267e1136a3
nabila <nabilatajrin@gmail.com>
2020-02-18 16:23:43 +0600

    04:24PM 18FEB2020 update

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -11,6 +11,9 @@ from profiles_api import serializers
 from profiles_api import models
 from profiles_api import permissions
 
+from rest_framework.authtoken.views import ObtainAuthToken
+from rest_framework.settings import api_settings
+
 
 # Create your views here.
 class HelloApiView(APIView):
@@ -116,3 +119,19 @@ class UserProfileViewSet(viewsets.ModelViewSet):
     #add search profile features
     filter_backends = (filters.SearchFilter,)
     search_fields = ('name', 'email',)
+
+
+class UserLoginApiView(ObtainAuthToken):
+   """Handle creating user authentication tokens"""
+   renderer_classes = api_settings.DEFAULT_RENDERER_CLASSES
+
+
+class UserProfileFeedViewSet(viewsets.ModelViewSet):
+    """Handles creating, reading and updating profile feed items"""
+    authentication_classes = (TokenAuthentication,)
+    serializer_class = serializers.ProfileFeedItemSerializer
+    queryset = models.ProfileFeedItem.objects.all()
+
+    def perform_create(self, serializer):
+        """Sets the user profile to the logged in user"""
+        serializer.save(user_profile=self.request.user)
---
+++ venv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/media/user/A/github/django-projects/django-profiles-rest-api/venv/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
